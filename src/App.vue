1c45edd6098995798a5614789dbf67133bdca64d
emoises <eduardo.moises@gmail.com>
2022-05-23 20:55:59 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <PostAtom>
+      
+   </PostAtom>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostAtom from "./components/molecules/PostAtom.vue"
 export default {
   name: 'App',
+  data() {
+    return{
+      title: "Eduardo Moisés da Silva"
+    }
+  },
   components: {
-    HelloWorld
-  }
+    PostAtom
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/molecules/PostAtom.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="post">
+   <div class="post-header"></div>
+   <div class="post-body"></div>
+   <div class="post-footer"></div>
+ </div>
+</template>
+
+<style scoped>
+  .post {
+    margin: 15px auto;
+  }
+
+  .post-header{
+    padding: 5px 12px;
+    background-color: #eee;
+  }
+
+  .post-body{
+    font-size: 16px;
+  }
+
+  .post-footer{
+    font-style: italic;
+  }
+</style>
